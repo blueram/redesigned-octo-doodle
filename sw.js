513f805c15cfb789118a17fa5b1ade51751bb9a8
blueram <64474406+blueram@users.noreply.github.com>
2026-07-19 09:08:55 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,5 @@
-const CACHE="moveox-v1";
+const CACHE="minigame-v2";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener("fetch",e=>{
-  if(e.request.method!=="GET") return;
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
-});
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
